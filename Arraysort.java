009bce620feaa08c68c9d09f158c604105e87406
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2021-01-05 15:26:41 +0100

    Sorting array in java

---
+++ Arraysort.java
@@ -0,0 +1,15 @@
+package projectone;
+
+import java.util.Arrays;
+
+public class Arraysort {
+	public static void main(String[] args) {
+		int arr[]= {41,23,54,20,22,12};
+		Arrays.sort(arr);
+		int i;
+		for ( i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+	}
+	
+	}
+}
